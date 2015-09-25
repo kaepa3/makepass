7fb49e4a7656097cf8c95155018b14254373a169
山崎孝洋 <tak_3_zaki3@yahoo.co.jp>
2015-09-25 16:53:02 +0900

    modify route: add default setting

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'makepass/show'
   post 'makepass/show'
   post 'makepass/update'
+  root :to => 'makepass#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
